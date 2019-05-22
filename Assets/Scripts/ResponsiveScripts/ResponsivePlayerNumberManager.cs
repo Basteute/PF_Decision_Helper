cab2578050ebb7d64db9f99fcb58cfb042d3629d
Basteute <36729725+Basteute@users.noreply.github.com>
2019-05-22 17:08:54 +0200

    Undo button enlarged

--- Assets/Scripts/ResponsiveScripts/ResponsivePlayerNumberManager.cs
+++ Assets/Scripts/ResponsiveScripts/ResponsivePlayerNumberManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ResponsivePlayerNumberManager : MonoBehaviour
 {
+    public GameObject mainCanvas;
     public GameObject panel;
     public GameObject rightBtn;
     public GameObject txtPlayerNumber;
@@ -13,15 +14,19 @@ public class ResponsivePlayerNumberManager : MonoBehaviour
     void Start()
     {
         Vector2 panelSize = panel.GetComponent<RectTransform>().rect.size;
+        Vector2 mainCanvasSize = mainCanvas.GetComponent<RectTransform>().rect.size;
+
+        // Change the size of the panel
+        //panel.GetComponent<RectTransform>().sizeDelta = new Vector2(mainCanvasSize.x * 0.8f, mainCanvasSize.y * 0.3f);
 
         // Change size of the upArrow
-        rightBtn.GetComponent<RectTransform>().sizeDelta = new Vector2(panelSize.x * 0.1f, panelSize.y * 0.5f);
+        rightBtn.GetComponent<RectTransform>().sizeDelta = new Vector2(panelSize.x * 0.25f, panelSize.y * 0.5f);
 
         // Change size of the card
-        txtPlayerNumber.GetComponent<RectTransform>().sizeDelta = new Vector2(panelSize.x * 0.7f, panelSize.y * 0.8f);
+        txtPlayerNumber.GetComponent<RectTransform>().sizeDelta = new Vector2(panelSize.x * 0.5f, panelSize.y * 0.8f);
 
         // Change size of the downArrow
-        leftBtn.GetComponent<RectTransform>().sizeDelta = new Vector2(panelSize.x * 0.1f, panelSize.y * 0.5f);
+        leftBtn.GetComponent<RectTransform>().sizeDelta = new Vector2(panelSize.x * 0.25f, panelSize.y * 0.5f);
     }
 
 }
