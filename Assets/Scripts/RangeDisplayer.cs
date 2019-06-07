e97435eab2e38e38138ad3bd2e29a73fc5d7dd27
Basteute <36729725+Basteute@users.noreply.github.com>
2019-06-07 08:44:19 +0200

    Unifished work about ranges

---
+++ Assets/Scripts/RangeDisplayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeDisplayer : MonoBehaviour
+{
+	public GameObject mainPanel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//createEmptyRangeArray();
+	}
+
+	private void createEmptyRangeArray()
+	{
+		Vector2 mainPanelSize = mainPanel.GetComponent<RectTransform>().rect.size;
+		GameObject originalHandObject = GameObject.Find("HandAA");
+
+		// Set the size and position of the object to clone
+		originalHandObject.GetComponent<RectTransform>().sizeDelta =
+				new Vector2(mainPanelSize.x * 0.0006f, mainPanelSize.y * 0.0006f);
+
+		originalHandObject.GetComponent<RectTransform>().anchoredPosition =
+				new Vector2(mainPanelSize.x * 0.006f, mainPanelSize.y * 0.006f);
+
+	}
+}
